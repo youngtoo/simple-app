969055fa8fc8a722a0db26a9fb2909bf15fe4a4c
Ian Too <ianktoo@gmail.com>
2024-07-28 00:50:45 +0300

    Added a new class.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
+import duke.choice.Clothing;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        double tax = 0.2;
+        double total, totalTax;
+
         System.out.println("Hello, World!");
+
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+
+        // Ian bought 3 items
+
+        // Item 1
+        item1.size = "S";
+        item1.description = "Blue Jacket";
+        item1.price = 20.9;
+
+        // Item 2
+        item2.size = "S";
+        item2.description = "Orange T-Shirt";
+        item2.price = 10.5;
+
+        // Bought 2 Tshirts and 1 Jacket
+        totalTax = tax * ( ( 2 * item2.price ) + item1.price );
+        total = ( totalTax + ( ( 2 * item2.price ) + item1.price ));
+
+        System.out.println("You bought: 2 " + item2.description + " and 1 " + item1.description);
+        System.out.println("Total purchases: " + total);
+
     }
 }
---
+++ src/duke/choice/Clothing.java
@@ -0,0 +1,12 @@
+package duke.choice;
+
+public class Clothing {
+
+    /**
+     * Properties: description, price , size
+     */
+    public String description, size = "M";
+    public double price;
+
+
+}
