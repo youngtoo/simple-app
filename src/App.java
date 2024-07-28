06c5b714feaf952f84620497a9bf70584e64bffe
Ian Too <ianktoo@gmail.com>
2024-07-28 11:33:45 +0300

    Added a switch statement to determinte the customer size based on a measurement.

--- src/App.java
+++ src/App.java
@@ -28,6 +28,29 @@ public class App {
 
         System.out.println("Item 2: " + item2.description + ", " + item2.size + ", " + item2.price);
 
+
+        //  Clothign array
+        Clothing[] clothes = new Clothing[3];
+
+        clothes[0] = new Clothing();
+        clothes[0].price = 100;
+        clothes[0].description = "White Shoes";
+        clothes[0].size = "S";
+
+        clothes[1] = new Clothing();
+        clothes[1].price = 150;
+        clothes[1].description = "Handbag";
+        clothes[1].size = "M";
+
+        clothes[2] = new Clothing();
+        clothes[2].size = "X";
+        clothes[2].description ="Underwear";
+        clothes[2].price = 12.99;
+
+        System.out.println(clothes[0].description + " | " + clothes[1].description + " | " + clothes[2].description);
+        System.out.println(clothes[0].size + " | " + clothes[1].size + " | " + clothes[2].size);
+        System.out.println(clothes[0].price+ " | " + clothes[1].price + " | " + clothes[2].price);
+
         // Bought 2 Tshirts and 1 Jacket
         totalTax = tax * ( ( 2 * item2.price ) + item1.price );
         total = ( totalTax + ( ( 2 * item2.price ) + item1.price ));
@@ -60,6 +83,10 @@ public class App {
         }
 
         System.out.println("Customer: " + customer1.customerName + " has size " + customer1.size);
+
+       for (Clothing clothing : clothes) {
+        System.out.println(clothing.description + ", " + clothing.price);
+       }
 
     }
 }
